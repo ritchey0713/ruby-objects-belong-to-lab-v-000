1cbdba2122ecfb6f9f092070f4c11afd317458ef
David Ritchey <ritchey0713@gmail.com>
2018-04-03 17:58:15 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,9 +2,9 @@ class Post
   
   attr_accessor :title, :author  
   
-  def initialize 
-    @title = title 
-    @author = author 
-  end 
+    def initialize 
+      @title = title 
+      @author = author 
+    end 
   
 end 
